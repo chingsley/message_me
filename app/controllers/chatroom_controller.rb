67d26667477762b19a61263de228ea78121237dd
chingsley <eneja.kc@gmail.com>
2019-07-14 00:32:27 +0100

    implement redirect at the controller level

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,6 +1,6 @@
 class ChatroomController < ApplicationController
-  before_action :require_user, only: [:index]
-  
+  before_action :require_user
+
   def index
     @messages = Message.all
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  before_action :redirect_to_homepage_if_logged_in, only: [:new, :create]
   
   def new
   end
@@ -19,6 +20,15 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     flash[:success] = "You have successfully logged out"
     redirect_to login_path
+  end
+
+  private
+
+  def redirect_to_homepage_if_logged_in
+    if logged_in?
+      flash[:info] = "You are already logged in"
+      redirect_to root_path
+    end
   end
   
 end
