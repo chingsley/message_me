0b2184ed5a495f8c9983e3eef45ec3ad63717ac4
chingsley <eneja.kc@gmail.com>
2019-07-14 21:12:55 +0100

    - write function to submit message by hitting enter - hide the send button - define custom scope to return only the most recent 20 messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :user
   validates :body, presence: true
+  scope :last_twenty_messages, -> { order(:created_at).last(20) }  # Get the last 20 messages, order them my the created_at column
 end
