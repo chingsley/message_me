27e8ecdaf8016f61e587075bc09a550459b5688e
chingsley <eneja.kc@gmail.com>
2019-07-15 17:17:27 +0100

    upgrade error messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
 
   def require_user
     if !logged_in?
-      flash[:error] = "You must be logged in to perofm that action"
+      flash[:error] = "Please login to continue"
       redirect_to login_path
     end
   end
